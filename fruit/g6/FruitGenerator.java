7578f2aaa6ba2f4872352edda8478931cf5e2114
Jonathan Balsano <jrbalsano@gmail.com>
2013-10-22 21:43:44 -0400

    Initial deliverable framework

---
+++ fruit/g6/FruitGenerator.java
@@ -0,0 +1,7 @@
+package fruit.g6;
+
+public class FruitGenerator implements fruit.sim.FruitGenerator {
+  public int[] generate(int nplayers, int bowlsize) {
+    int nFruits = nPlayers * bowlsize;
+  }
+}
---
+++ fruit/g6/Player.java
@@ -0,0 +1,30 @@
+package fruit.g6;
+
+public class Player extends fruit.sim.Player {
+  public final int NFRUIT = 12;
+  int[] mExpDistrib = new int[NFRUIT];
+  int mPreferences = new int[NFRUIT];
+  
+  public void init(int nplayers, int[] pref) {
+
+  }
+
+  public boolean pass(int[] bowl, int bowlId, int round,
+                      boolean canPick,
+                      boolean musTake) {
+   
+  }
+  
+  private getBowlScore(int[] bowl) {
+    
+  }
+  
+  /**
+   * Average the newBowl with the current expected distribution
+   * @param newBowl
+   */
+  private generateDistribution(int[] newBowl) {
+    
+  }
+  
+}
